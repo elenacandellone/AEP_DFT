8802ce76fa0c13319040410d616e65491833f47e
Elena Candellone <60847293+elenacandellone@users.noreply.github.com>
2021-07-02 11:28:13 +0200

    Add files via upload

---
+++ k_smearing_plots/en.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+kgrid="2 4 6 8 10 12 14"
+smearing="0.10 0.09 0.08 0.07 0.06 0.05 0.04 0.03 0.02 0.01"
+
+for k in $kgrid 
+do
+for sigma in $smearing
+do 
+printf "${k}"
+printf "	"
+printf "${sigma}"
+awk 'BEGIN{FS=" *= *"} /!    total energy/{split($2,a," +");print "  " a[1]}' pb_${k}_${sigma}.scf.out 
+done
+done > energy.dat
+
